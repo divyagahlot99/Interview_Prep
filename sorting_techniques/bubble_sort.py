89508e9604b555919f72b73c355e438ad794fa05
Divya Gahlot <gahlotdivya1019@gmail.com>
2020-06-16 18:01:34 +0530

    Update bubble_sort.py

--- sorting_techniques/bubble_sort.py
+++ sorting_techniques/bubble_sort.py
@@ -1,1 +1,10 @@
-
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+ 
+arr = [4, 2, 8, 6, 4, 1, 0, 15, 4, 3]
+print(bubble_sort(arr))
